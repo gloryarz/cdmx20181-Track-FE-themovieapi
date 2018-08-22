d6347771da958fc4a990507b8c9bb474a6607f12
gloryarz <gloryarz@gmail.com>
2018-08-22 09:23:32 -0500

    Imprimiendo primer búsqueda

--- docs/src/js/app.js
+++ docs/src/js/app.js
@@ -28,10 +28,55 @@ fetch(url)
     }
 })
 
+let moviesArr = [];
+
 const gettingMovies = (search) => {
     for(numbers in search){
         let movies = search[numbers];
-        let movieTitle = movies.Title;
-        console.log(movies);
+        let movieArr = [movies.imdbID, movies.Title, movies.Year, movies.Type, movies.Poster]
+        moviesArr.push(movieArr);
     }
+    print();
 }
+
+
+
+const print = () => {
+    moviesArr.forEach(element => {
+        console.log(element[1])
+        let movieID = element[0];
+        console.log(movieID)
+        let movieTitle = element[1];
+        let movieYear = element[2];
+        let movieType = element[3];
+        let moviePoster = element[4];
+        inner.innerHTML += `
+        <img src="${moviePoster}">
+        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#${movieID}">
+          Ver más
+        </button>
+        
+        <!-- Modal -->
+        <div class="modal fade" id="${movieID}" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+          <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalCenterTitle">${movieTitle}</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body">
+              <img src="${moviePoster}">
+              Año : ${movieYear}
+              Tipo : ${movieType}
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+              </div>
+            </div>
+          </div>
+        </div>`
+    });
+};
